f5b6dd57dd38b545e104b674edf9fe190cc7902d
matyb <mathewkbentley@gmail.com>
2017-10-07 14:41:06 -0400

    getting there

--- vars/curl.groovy
+++ vars/curl.groovy
@@ -1,3 +1,3 @@
 def String call(url, branches = [env.BRANCH_NAME], curlArgs = '-X GET', continueFn = { branch, exception -> true}) {
-  return new org.bratcher.Bratcher().curl(url, branches, curlArgs, continueFn)
+  return new Bratcher().curl(url, branches, curlArgs, continueFn)
 }

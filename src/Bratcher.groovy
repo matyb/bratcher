960305039680d7992756665ebb83b573ad675545
matyb <mathewkbentley@gmail.com>
2017-10-07 21:14:13 -0400

    handling 404 without exception, sh process returning non-zero doesn't throw inside a pipeline lib

--- src/Bratcher.groovy
+++ src/Bratcher.groovy
@@ -3,6 +3,11 @@ def String curl(url, branches, curlArgs = '-f -X GET', continueFn = { branch, ex
     try {
       url = url.replace('$branch', branches.head())
       def val = sh(returnStdout: true, script: "curl " + curlArgs + " '$url'")
+      if(!val || val.isEmpty()){
+        if(continueFn(branches.head(), x)){
+          curl(url, branches.tail(), curlArgs, continueFn)
+        }
+      }
       return val
     } catch (Exception x) {
       if(continueFn(branches.head(), x)){

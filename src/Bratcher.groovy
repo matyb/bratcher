a348c8de6df5b4cecca6220d47db7f2c67cd92eb
matyb <mathewkbentley@gmail.com>
2017-10-07 22:14:04 -0400

    rounding out units from integration testing within jenkins

--- src/Bratcher.groovy
+++ src/Bratcher.groovy
@@ -3,16 +3,10 @@ def String curl(url, branches, curlArgs = '-f -X GET', continueFn = { branch, ex
     try {
       subUrl = url.replace('$branch', branches.head())
       def val = sh(returnStdout: true, script: "curl " + curlArgs + " '$subUrl'")
-      if(!val || val.isEmpty()){
-        if(continueFn(branches.head(), x)){
-          echo "empty response... ${branches.tail()}"
-          curl(url, branches.tail(), curlArgs, continueFn)
-        }
-      }
       return val
     } catch (Exception x) {
       if(continueFn(branches.head(), x)){
-        echo "exception response... ${branches.tail()}"
+        echo "exception response... ${branches.tail()}".toString()
         curl(url, branches.tail(), curlArgs, continueFn)
       }
     }

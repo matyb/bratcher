5bea76a16858436eb821bd9ac90f3e75ff8cb7e9
matyb <mathewkbentley@gmail.com>
2017-10-07 21:15:21 -0400

    missing -f passed to curl originating in vars

--- vars/curl.groovy
+++ vars/curl.groovy
@@ -1,3 +1,3 @@
-def String call(url, branches = [env.BRANCH_NAME], curlArgs = '-X GET', continueFn = { branch, exception -> true}) {
+def String call(url, branches = [env.BRANCH_NAME], curlArgs = '-f -X GET', continueFn = { branch, exception -> true}) {
   return new Bratcher().curl(url, branches, curlArgs, continueFn)
 }
